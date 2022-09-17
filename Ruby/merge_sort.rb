61dfaded6ad3da8f1e887501c5ce94db2a8114c1
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2022-09-17 07:01:07 +0600

    Added merge sort algorithm in Ruby

---
+++ Ruby/merge_sort.rb
@@ -0,0 +1,37 @@
+def merge(left_array, right_array)
+    if right_array.empty?
+        return left_array
+    end
+
+    if left_array.empty?
+        return right_array
+    end
+
+    smallest_number = if left_array.first <= right_array.first
+        left_array.shift
+    else
+        right_array.shift
+    end
+
+    recursive = merge(left_array, right_array)
+
+    [smallest_number].concat(recursive)
+end
+
+def merge_sort(array)
+    size = array.length
+    if size <= 1
+        return array
+    end
+
+    mid = (size / 2).round
+    left = array.take(mid)
+    right = array.drop(mid)
+
+    sorted_left = merge_sort(left)
+    sorted_right = merge_sort(right)
+
+    return merge(sorted_left, sorted_right)
+end
+
+puts merge_sort([5, 4, 2, 1])

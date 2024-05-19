c62a8142b6002645b33eacfdae6aedec07e22a08
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-05-19 06:48:50 +0600

    Added linear search in JavaScript

---
+++ JavaScript/Algorithms/linearSearch.js
@@ -0,0 +1,15 @@
+const linearSearch = (array, item) => {
+    let idx = 0;
+
+    while(idx < array.length) {
+        if(array[idx] === item) {
+            return idx;
+        }
+        idx++;
+    }
+    return -1;
+}
+
+let arr = [1, 2, 3, 4, 5];
+
+console.log(linearSearch(arr, 4));

6b14667d7bbd83aa20b1e5168bfb3d79db86fb9b
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-05-19 07:01:36 +0600

    Added selection sort algorithm in JavaScript

---
+++ JavaScript/Algorithms/selectionSort.js
@@ -0,0 +1,20 @@
+const selectionSort = (array) => {
+    for(let i=0;i<array.length;i++) {
+        let min = i;
+        for(let j=i+1;j<array.length;j++) {
+            if(array[min] > array[j]) {
+                min = j;
+            }
+        }
+        if(i !== min) {
+            let temp = array[i];
+            array[i] = array[min];
+            array[min] = temp;
+        }
+    }
+    return array;
+}
+
+let array = [5, 4, 3, 2, 1];
+
+console.log(selectionSort(array));

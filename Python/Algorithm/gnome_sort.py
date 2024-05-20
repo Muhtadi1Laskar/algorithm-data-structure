4271236850ff2b4332fc9cd3c04104bacaad8853
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-05-21 03:07:21 +0600

    Added Gnome Sort in python

---
+++ Python/Algorithm/gnome_sort.py
@@ -0,0 +1,16 @@
+def gnome_sort(li: list[int]) -> list[int]:
+    index: int = 0
+
+    while index < len(li):
+        if index == 0:
+            index += 1
+        elif li[index] >= li[index-1]:
+            index += 1
+        else:
+            temp = li[index]
+            li[index] = li[index-1]
+            li[index-1] = temp
+            index -= 1
+    return li
+
+print(gnome_sort([5, 4, 3, 2, 1]))

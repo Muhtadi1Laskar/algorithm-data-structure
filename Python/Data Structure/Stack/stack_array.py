0c0bb306a8db732fd094d6101417ab24d73d4770
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-06-06 18:00:44 +0600

    Added pop mehtod to stack_array:

--- Python/Data Structure/Stack/stack_array.py
+++ Python/Data Structure/Stack/stack_array.py
@@ -17,11 +17,38 @@ class Stack:
         self.stack[0] = value
         self.length += 1
     
+    def pop(self):
+        item = self.stack[0]
+
+        for i in range(0, self.length-1):
+            self.stack[i] = self.stack[i + 1]
+
+        self.stack[self.length-1] = 0
+        self.length -= 1
+
+        return item
+    
     def print_list(self):
         return self.stack
 
 
 stack = Stack()
+
+stack.add(1)
+stack.add(2)
+stack.add(3)
+stack.add(4)
+stack.add(5)
+
+print(stack.print_list())
+
+stack.pop()
+stack.pop()
+stack.pop()
+stack.pop()
+stack.pop()
+
+print(stack.print_list())
 
 stack.add(1)
 stack.add(2)

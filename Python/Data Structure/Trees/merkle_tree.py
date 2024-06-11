815be8307c956da78740f4041dd788ec7f51d2d5
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-06-11 12:23:22 +0600

    Added Merkle Tree in Python

--- Python/Data Structure/Trees/merkle_tree.py
+++ Python/Data Structure/Trees/merkle_tree.py
@@ -1,19 +1,19 @@
 import hashlib
 
 class MerkleTree:
-    def __init__(self, data_list):
-        self.leaves = [self.hash_data(data) for data in data_list]
+    def __init__(self, value):
+        self.leaves = [self.hash_data(leave) for leave in value]
         self.root = self.build_tree(self.leaves)
     
     def hash_data(self, data):
         return hashlib.sha256(data.encode('utf-8')).hexdigest()
-
+    
     def build_tree(self, leaves):
         if not leaves:
             return None
         
         current_level = leaves[:]
-
+        
         while len(current_level) > 1:
             if len(current_level) % 2 != 0:
                 current_level.append(current_level[-1])
@@ -29,10 +29,8 @@ class MerkleTree:
     def get_root(self):
         return self.root
 
-
 data = ['C', 'JavaScript', 'Python', 'Haskell', 'Go', 'Odin']
 
 merkle_tree = MerkleTree(data)
 
 print(merkle_tree.root)
-

b300ca32c574d5509ea48ab4b6db22616271b93a
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-06-12 12:20:04 +0600

    Added __buildTreeRec() method to merkle tree

--- Python/Data Structure/Trees/merkle_tree.py
+++ Python/Data Structure/Trees/merkle_tree.py
@@ -7,25 +7,48 @@ class Node:
         self.value = value
         self.content = content
         self.is_copied = is_copied
-    
+
     def hash(value):
-        return hashlib.sha256(value.encode('utf08')).hexdigest()
+        return hashlib.sha256(value.encode("utf-8")).hexdigest()
 
     def __str__(self):
         return str(self.value)
 
     def copy(self):
         return Node(self.left, self.right, self.value, self.content, True)
-    
+
 
 class MerkleTree:
     def __init__(self, values):
         self.__buildTree(values)
-    
+
     def __buildTree(self, values):
         leaves = [Node(None, None, Node.hash(e), e) for e in values]
 
         if len(leaves) % 2 != 0:
             leaves.append(leaves[-1].copy())
-        
+
         self.root = self.__buildTreeRec(leaves)
+
+    def __buildTreeRec(self, nodes):
+        if len(nodes) % 2 != 0:
+            nodes.append(nodes[-1].copy())
+
+        half = len(nodes) // 2
+
+        if len(nodes) == 2:
+            value = Node.hash(nodes[0].value + nodes[1].value)
+            content = f"{nodes[0].content} + {nodes[1].content}"
+            return Node(nodes[0], nodes[1], value, content)
+
+        left = self.__buildTreeRec(nodes[:half])
+        right = self.__buildTreeRec(nodes[half:])
+        value = Node.hash(left.value + right.value)
+        content = f"{left.content} + {right.content}"
+
+        return Node(left, right, value, content)
+    
+
+values = ['C', 'JavaScript', 'Python', 'Haskell', 'Go', 'Odin']
+merkle_tree = MerkleTree(values)
+print("Merkle Root:", merkle_tree.root)

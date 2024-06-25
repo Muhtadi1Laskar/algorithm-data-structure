35b3f8b1b14bc110c917798d2eba5dd06406fa83
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-06-25 17:16:40 +0600

    Added bubble sort in Julia

---
+++ Julia/Algorithms/bubble_sort.jl
@@ -0,0 +1,12 @@
+function bubble_sort(array)
+    for i in 1:length(array)
+        for j in i+1:length(array)
+            if array[i] > array[j]
+                array[i], array[j] = array[j], array[i]
+            end
+        end
+    end
+    return array
+end
+
+println(bubble_sort([10, 9, 8, 7, 6, -5, 4, 3, 2, 1]))

6e3025c7f0fc78a0101b7a4cff15692a2d00c96c
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-06-25 17:27:50 +0600

    Added insertion sort in Julia

---
+++ Julia/Algorithms/insertion_sort.jl
@@ -0,0 +1,18 @@
+function insertion_sort(array)
+    for i in 1:length(array)
+        key = array[i]
+        j = i - 1
+
+        while j >= 1 && array[j] > key
+            array[j+1] = array[j]
+            j -= 1
+        end
+
+        array[j+1] = key
+    end
+    return array
+end
+
+array = [5, 4, 3, 2, 10]
+
+println(insertion_sort(array))

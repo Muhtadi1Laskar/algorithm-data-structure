61a06280f1509423b1234024dbb0daeff60d2e7e
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-07-07 16:26:08 +0600

    Added resucrion fibonacci

--- Julia/Recursions/fibonacci.jl
+++ Julia/Recursions/fibonacci.jl
@@ -5,3 +5,19 @@ function fib(n)
     return fib(n-1) + fib(n-2)
 end
 
+function fib_loop(n)
+    if n == 0
+      return 0 
+    end
+  
+    last = 0
+    first = 1
+  
+    for i in 1:n-1 
+      last, first = first, last + first
+    end
+  
+    return first
+  end
+  
+  println(fib_loop(10))

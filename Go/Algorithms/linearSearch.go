1ab48bf204693909c8cca90f2ca17988862ee669
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-09-09 21:25:43 +0600

    Changed the for loop of linear search to range iterator

--- Go/Algorithms/linearSearch.go
+++ Go/Algorithms/linearSearch.go
@@ -3,15 +3,15 @@ package main
 import "fmt"
 
 func linearSearch(array []int, item int) int {
-	for i := 0; i < len(array); i++ {
-		if array[i] == item {
-			return i;
+	for idx, elem := range array {
+		if elem == item {
+			return idx;
 		}
 	}
 	return -1;
 }
 
 func main() {
-	array := []int{1, 2, 3, 4, 5, 6, 7, 8, 10};
-	fmt.Println(linearSearch(array, 5));
+	array := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	fmt.Println(linearSearch(array, 10));
 }

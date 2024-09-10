979c593688efb63567402c4e81600419a00746a7
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-09-10 18:47:56 +0600

    Added binary search in Go

--- Go/Algorithms/linearSearch.go
+++ Go/Algorithms/linearSearch.go
@@ -2,10 +2,19 @@ package main
 
 import "fmt"
 
-func linearSearch(array []int, item int) int {
-	for idx, elem := range array {
-		if elem == item {
-			return idx;
+func binarySearch(array []int, item int) int {
+	var l int = 0;
+	var r int = len(array) - 1;
+
+	for l <= r {
+		var mid int = l + (r-l) / 2;
+
+		if array[mid] == item {
+			return mid;
+		} else if array[mid] > item {
+			r = mid - 1;
+		} else {
+			l = mid + 1;
 		}
 	}
 	return -1;
@@ -13,5 +22,5 @@ func linearSearch(array []int, item int) int {
 
 func main() {
 	array := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-	fmt.Println(linearSearch(array, 10));
+	fmt.Println(binarySearch(array, 10));
 }

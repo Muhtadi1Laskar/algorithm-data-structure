8bcae1b5499ba979e545d039ae101790324d6882
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-09-10 22:43:37 +0600

    Fixed the binary search in Go

---
+++ Go/Algorithms/binarySearch.go
@@ -0,0 +1,19 @@
+package main
+
+func binarySearch(array []int, item int) int {
+	var l int = 0;
+	var r int = len(array) - 1;
+
+	for l <= r {
+		var mid int = l - (r-1) / 2;
+
+		if array[mid] == item {
+			return mid;
+		} else if array[mid] > item {
+			r = mid - 1;
+		} else {
+			l = mid + 1;
+		}
+	}
+	return -1;
+}

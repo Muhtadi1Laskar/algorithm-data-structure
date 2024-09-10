d964ba0f27c5ff85963e9b9b52cb87a0c93ff771
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-09-10 22:39:58 +0600

    Fixed linear search in Go

--- Go/Algorithms/linearSearch.go
+++ Go/Algorithms/linearSearch.go
@@ -1,21 +1,10 @@
 package main 
 
-import "fmt"
-
-func bubbleSort(array []int) []int {
-	for i := 0; i < len(array); i++ {
-		for j := i+1; j < len(array); j++ {
-			if array[i] > array[j] {
-				temp := array[i];
-				array[i] = array[j];
-				array[j] = temp;
-			}
+func linearSearch(array []int, item int) int {
+	for idx, elem := range array {
+		if elem == item {
+			return idx;
 		}
 	}
-	return array;
-}
-
-func main() {
-	array := []int{5, 4, 3, 2, 1};
-	fmt.Println(bubbleSort(array));
+	return -1;
 }

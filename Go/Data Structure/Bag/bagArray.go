71efe905819b74e5a9188c9a38ca95ddb4b93665
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-10-02 03:38:44 +0600

    Added the Add() Len() and Log() method

--- Go/Data Structure/Bag/bagArray.go
+++ Go/Data Structure/Bag/bagArray.go
@@ -10,8 +10,28 @@ func newBag() *Bag {
 	return &Bag{ arr: []string{}}
 }
 
+func (b *Bag) Add(item string) []string {
+	b.arr = append(b.arr, item);
+	return b.arr;
+}
 
+func (b *Bag) Len() int {
+	return len(b.arr);
+}
+
+func (b *Bag) Log() {
+	for i := 0; i < len(b.arr); i++ {
+		fmt.Println(b.arr[i]);
+	}
+}
 
 func main() {
-	fmt.Println("Hello World")
+	arr := newBag();
+	arr.Add("Saitama");
+	arr.Add("Luffy");
+	arr.Add("Zoro");
+	arr.Add("Billy Bat");
+	arr.Add("Zenetisu");
+
+	arr.Log();
 }

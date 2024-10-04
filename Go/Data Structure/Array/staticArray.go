2a8bfd45b23099cb5a2333d59889db19c9d33a9f
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-10-04 14:06:37 +0600

    Added Traverse() method to static array

--- Go/Data Structure/Array/staticArray.go
+++ Go/Data Structure/Array/staticArray.go
@@ -24,8 +24,21 @@ func (array *Array) Push(item int) {
 	return;
 }
 
+func (array *Array) Traverse(fn func(int) int) []int {
+	var resultArray []int;
+
+	for i := 0; i < len(array.array); i++ {
+		resultArray = append(resultArray, fn(array.array[i]));
+	}
+	return resultArray;
+}
+
 func (array *Array) GetLength() int {
 	return array.len;
+}
+
+func double(num int) int {
+	return num * num;
 }
 
 func main() {
@@ -37,4 +50,6 @@ func main() {
 
 	fmt.Println(array.array);
 	fmt.Println(array.len);
+
+	fmt.Println(array.Traverse(double));
 }

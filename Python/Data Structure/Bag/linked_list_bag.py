64b981bfe4f4e11c6cb69ca0758e38bedaed9dd2
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-10-10 18:19:42 +0600

    Added Push() method to bag data structure

--- Python/Data Structure/Bag/linked_list_bag.py
+++ Python/Data Structure/Bag/linked_list_bag.py
@@ -11,6 +11,39 @@ class Bag:
 
     def is_empty(self):
         return not self.head
+    
+    def push(self, value):
+        new_node = Node(value)
+
+        if self.is_empty():
+            self.head = new_node
+            self.tail = self.head
+            self.len += 1
+            return
+
+        self.tail.next = new_node
+        self.tail = new_node
+        self.len += 1
+        return
+    
+    def log(self):
+        if self.is_empty():
+            return None
+        current_node = self.head
+        result = []
+
+        while current_node:
+            result.append(current_node.value)
+            current_node = current_node.next
+        
+        return " ---> ".join(result)
 
 
 bag = Bag()
+
+bag.push("Saitama")
+bag.push("Luffy")
+bag.push("Naruto")
+bag.push("Ichigo")
+
+print(bag.log())

1bbfbfb6af8045b2a7e9dc8532ac539aa48056a4
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-10-11 21:07:56 +0600

    Added method to queue class

--- Python/Data Structure/Queue/array_queue.py
+++ Python/Data Structure/Queue/array_queue.py
@@ -6,6 +6,12 @@ class CircularQueue:
         this.rear = 0
         this.count = 0
     
+    def is_empty(this):
+        return this.count == 0
+    
+    def is_full(this):
+        return this.count == this.size
+    
 
 
 queue = CircularQueue(10)

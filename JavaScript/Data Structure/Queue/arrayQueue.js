ed3185cca0ca2f08fb506fd0b4b726aeeedaa358
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-10-11 16:02:22 +0600

    Added enqueue() method to queue

--- JavaScript/Data Structure/Queue/arrayQueue.js
+++ JavaScript/Data Structure/Queue/arrayQueue.js
@@ -1,10 +1,20 @@
 class CircularQueue {
     constructor(size) {
         this.size = size;
-        this.array = [];
+        this.array = new Array(this.size);
         this.front = 0;
         this.rear = 0;
         this.count = 0;
+    }
+
+    enqueue(value) {
+        if(this.isFull()) {
+            return null;
+        }
+        this.array[this.rear] = value;
+        this.rear = (this.rear + 1) % this.size;
+        this.count += 1;
+        return this;
     }
 
     isFull() {
@@ -17,3 +27,9 @@ class CircularQueue {
 }
 
 const queue = new CircularQueue(10);
+
+for(let i = 1; i <= 10; i++) {
+    queue.enqueue(i*2);
+}
+
+console.log(queue.array);

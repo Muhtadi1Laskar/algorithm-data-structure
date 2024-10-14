cae3f362c379b9f39245439a2329c858d8828c0f
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-10-14 15:50:41 +0600

    Changed code in main file under Go folder

--- Go/main.go
+++ Go/main.go
@@ -1,11 +1,7 @@
 package main
 
-// import "fmt"
+import "fmt"
 
-// func main() {
-// 	var staticArray = [3]int{1, 2, 3}
-
-// 	fmt.Println(&staticArray[0])
-// 	fmt.Println(&staticArray[1])
-// 	fmt.Println(&staticArray[2])
-// }
+func mian() {
+	fmt.Println("Hello World")
+}

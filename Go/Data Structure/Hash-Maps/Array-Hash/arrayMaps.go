8de70fdff8f6b1543998d5d5724257d4073ab72f
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-10-18 04:23:59 +0600

    Added Set() method to hash map in Go

--- Go/Data Structure/Hash-Maps/Array-Hash/arrayMaps.go
+++ Go/Data Structure/Hash-Maps/Array-Hash/arrayMaps.go
@@ -23,7 +23,21 @@ func NewHashMap(size int) *HashMap {
 	}
 }
 
+func (h *HashMap) Set(key string, value interface{}) {
+	var hashedKey uint32 = hash(key)
+	var index int = int(hashedKey) % h.size
 
+	for i := 0; i < len(h.array[index]); i++ {
+		if h.array[index][i].key == key {
+			h.array[index][i].value = value
+			return
+		}
+	}
+	h.array[index] = append(h.array[index], &KeyValuePair{
+		key: key,
+		value: value,
+	})
+}
 
 func hash(value string) uint32 {
 	h := fnv.New32a()
@@ -32,5 +46,29 @@ func hash(value string) uint32 {
 }
 
 func main() {
-	fmt.Println("Hello World")
+	hashMap := NewHashMap(10)
+
+	items := []struct{
+		key string
+		value interface{}
+	}{
+		{"Name", "Luffy"},
+		{"Occupation", "Pirate"},
+		{"Age", 20},
+		{"Favourite Food", "Meat"},
+		{"Devil Fruit User", true},
+		{"Rank", "Captain"},
+	}
+
+	for _, item := range items {
+		hashMap.Set(item.key, item.value)
+	}
+
+	for _, arr := range hashMap.array {
+		if len(arr) > 0 {
+			for _, pair := range arr {
+				fmt.Println(pair.key, pair.value)
+			}
+		} 
+	}
 }

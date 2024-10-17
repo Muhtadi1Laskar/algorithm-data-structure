cefebd8d9cb42a52f6da37e463704524b4a4f0fe
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-10-17 16:06:47 +0600

    Added set() method to linear_probe.py

--- JavaScript/Data Structure/Hash Map/Linear Probe Implementation/linearProbe.js
+++ JavaScript/Data Structure/Hash Map/Linear Probe Implementation/linearProbe.js
@@ -1,27 +1,43 @@
-import crypto from "crypto"
-
 class HashMap {
-    constructor(size=10) {
+    constructor(size = 10) {
         this.size = size;
-        this.array = new Array(size).fill([]);
+        this.array = new Array(size).fill(null);
     }
 
     static hash(key) {
         let hash = 0;
         for (let i = 0; i < key.length; i++) {
-          hash = ((hash << 5) - hash) + key.charCodeAt(i);
-          hash = hash & hash;
+            hash = ((hash << 5) - hash) + key.charCodeAt(i);
+            hash = hash & hash;
         }
         return hash;
-      }
+    }
 
     set(key, value) {
-        const hashedKey = HashMap.hash(key)
+        const hashedKey = HashMap.hash(key);
+        let index = hashedKey % this.size;
 
-        return hashedKey
+        while (this.array[index] !== null) {
+            index = (index + 1) % this.size;
+        }
+
+        this.array[index] = [key, value];
     }
 }
 
 const hashMap = new HashMap();
 
-console.log(hashMap.set("Name", "Luffy"));
+const data = [
+    ['Name', 'Luffy'],
+    ['Occupation', 'Pirate'],
+    ['Age', 20],
+    ['Favourite Food', 'Meat'],
+    ['Devil Fruit User', true]
+];
+
+for (let i = 0; i < data.length; i++) {
+    const [key, value] = data[i];
+    hashMap.set(key, value);
+}
+
+console.log(hashMap.array);

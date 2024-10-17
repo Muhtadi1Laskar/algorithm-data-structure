cb67043e9d62bde144611c282f43cf6cfed41129
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-10-17 16:19:01 +0600

    Added get() method to linearProbe.js

--- JavaScript/Data Structure/Hash Map/Linear Probe Implementation/linearProbe.js
+++ JavaScript/Data Structure/Hash Map/Linear Probe Implementation/linearProbe.js
@@ -8,9 +8,9 @@ class HashMap {
         let hash = 0;
         for (let i = 0; i < key.length; i++) {
             hash = ((hash << 5) - hash) + key.charCodeAt(i);
-            hash = hash & hash;
+            hash = hash & 0xffffffff;
         }
-        return hash;
+        return Math.abs(hash);
     }
 
     set(key, value) {
@@ -23,6 +23,20 @@ class HashMap {
 
         this.array[index] = [key, value];
     }
+
+    get(key) {
+        const hashedKey = HashMap.hash(key);
+        let index = hashedKey % this.size;
+        
+        while(this.array[index] !== null && this.array[index][0] !== key) {
+            index = (index + 1) % this.size;
+        }
+
+        if(this.array[index] === null) {
+            return null
+        }
+        return this.array[index][1];
+    }
 }
 
 const hashMap = new HashMap();
@@ -32,7 +46,8 @@ const data = [
     ['Occupation', 'Pirate'],
     ['Age', 20],
     ['Favourite Food', 'Meat'],
-    ['Devil Fruit User', true]
+    ['Devil Fruit User', true], 
+    ['Devil Fruit Name', "Gomu Gomu No Mi"]
 ];
 
 for (let i = 0; i < data.length; i++) {
@@ -40,4 +55,7 @@ for (let i = 0; i < data.length; i++) {
     hashMap.set(key, value);
 }
 
-console.log(hashMap.array);
+for(let i = 0; i < data.length; i++) {
+    const [key, value] = data[i];
+    console.log(hashMap.get(key));
+}

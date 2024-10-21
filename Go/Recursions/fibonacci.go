108d943f02d7cbde4dd074a7b3301309544237a3
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-10-21 20:32:08 +0600

    Added Fibonacci iterative code

--- Go/Recursions/fibonacci.go
+++ Go/Recursions/fibonacci.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/big"
+)
 
 func fib(n int) int {
 	if n <= 2 {
@@ -9,8 +12,23 @@ func fib(n int) int {
 	return fib(n-1) + fib(n-2)
 }
 
-func main() {
-	var number int = 10
+func fibIter(n int) *big.Int {
+	if n == 0 {
+		return big.NewInt(0)
+	}
+	last := big.NewInt(0)
+	next := big.NewInt(1)
 
-	fmt.Println(fib(number))
+	for i := 1; i < n; i++ {
+		temp := new(big.Int).Set(last)
+		last.Set(next)
+		next.Add(temp, next)
+	}
+	return next
+}
+
+func main() {
+	number := 200
+
+	fmt.Println(fibIter(number))
 }

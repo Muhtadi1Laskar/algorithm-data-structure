3e1b140999121c5f6038b3d8b4897502c30bd00d
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-11-01 18:11:15 +0600

    Added Search method

--- JavaScript/Data Structure/Inverted Index/invertedIndex.js
+++ JavaScript/Data Structure/Inverted Index/invertedIndex.js
@@ -6,29 +6,37 @@ class InvertedIndex {
     addDocument(doc) {
         const words = doc.Text.toLowerCase().split(' ');
 
-        for(let i = 0; i < words.length; i++) {
-            if(!this.index[words[i]]) {
-                this.index[words[i]] = [];
+        for (let i = 0; i < words.length; i++) {
+            const chars = words[i];
+
+            if (!this.index[chars]) {
+                this.index[chars] = [];
             }
-            if(!words[i].includes(doc.ID)) {
-                this.index[words[i]].push(doc.ID);
+            if (!chars.includes(doc.ID)) {
+                this.index[chars].push(doc.ID);
             }
         }
     }
 
-    
+    search(words) {
+        return this.index.hasOwnProperty(words.toLowerCase()) ?
+               this.index[words.toLowerCase()] : [];
+    }
 }
 
 const invertedIndex = new InvertedIndex();
 
 const docs = [
-    {ID: 1, Text: "The quick brown fox"},
-	{ID: 2, Text: "The quick blue fox"},
-	{ID: 3, Text: "The fast red fox"}
+    { ID: 1, Text: "The quick brown fox" },
+    { ID: 2, Text: "The quick blue fox" },
+    { ID: 3, Text: "The fast red fox" }
 ];
 
-for(let i = 0; i < docs.length; i++) {
+for (let i = 0; i < docs.length; i++) {
     invertedIndex.addDocument(docs[i]);
 }
 
-console.log(invertedIndex.index);
+console.log("Documents containing 'quick':", invertedIndex.search("quick"))
+console.log("Documents containing 'fox':", invertedIndex.search("fox"))
+console.log("Documents containing 'fast':", invertedIndex.search("fast"))
+console.log("Documents containing 'elephant':", invertedIndex.search("elephant"))

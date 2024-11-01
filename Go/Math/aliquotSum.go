8ddbfedb1bf95eb646b6e79f229e20df8c8a1083
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-11-01 20:41:28 +0600

    Added Aliquot Number in Go

--- Go/Math/aliquotSum.go
+++ Go/Math/aliquotSum.go
@@ -1,18 +1,12 @@
 package main
 
-import "fmt"
-
 func aliquotSum(n int) int {
 	var result int = 0
 
-	for i := 1; i <= n/2; i++ {
+	for i := 1; i <= n; i++ {
 		if n % i == 0 {
 			result += i
 		}
 	}
 	return result
-}
-
-func main() {
-	fmt.Println(aliquotSum(10))
 }

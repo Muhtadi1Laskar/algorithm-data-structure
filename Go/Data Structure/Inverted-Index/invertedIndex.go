df8a6d494e082f954911c820409c31f562d75b0d
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-11-01 17:01:30 +0600

    Added AddDocument() method

--- Go/Data Structure/Inverted-Index/invertedIndex.go
+++ Go/Data Structure/Inverted-Index/invertedIndex.go
@@ -14,6 +14,41 @@ type InvertedIndex struct {
 	index map[string][]int
 }
 
-func main() {
+func NewInvertedIndex() *InvertedIndex {
+	return &InvertedIndex{
+		index: make(map[string][]int),
+	}
+}
 
+func (li *InvertedIndex) AddDocument(doc Document) {
+	words := strings.Fields(strings.ToLower(doc.Text))
+
+	for _, word := range words {
+		if !contains(li.index[word], doc.ID) {
+			li.index[word] = append(li.index[word], doc.ID)
+		}
+	}
+}
+
+func contains(slice []int, item int) bool {
+	for _, v := range slice {
+		if v == item {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	docs := []Document{
+		{ID: 1, Text: "The quick brown fox"},
+		{ID: 2, Text: "The quick blue fox"},
+		{ID: 3, Text: "The fast red fox"},
+	}
+
+	invertedIndex := NewInvertedIndex()
+
+	for _, doc := range docs {
+		invertedIndex.AddDocument(doc)
+	}
 }

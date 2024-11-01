6b05b2523e2aba6bc6c6a21587bf5ad8f6b4b3e7
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-11-01 21:25:04 +0600

    Added Check Abundant Number method

--- Go/Math/aliquotSum.go
+++ Go/Math/aliquotSum.go
@@ -3,10 +3,26 @@ package main
 func aliquotSum(n int) int {
 	var result int = 0
 
-	for i := 1; i <= n; i++ {
+	for i := 1; i < n; i++ {
 		if n % i == 0 {
 			result += i
 		}
 	}
 	return result
+}
+
+func checkUntouchable(n int) bool {
+	for i := 1; i <= n*n; i++ {
+		if aliquotSum(i) == n {
+			return false
+		}
+	}
+	return true
+}
+
+func checkAbundant(n int) bool {
+	if aliquotSum(n) > n {
+		return true
+	}
+	return false
 }

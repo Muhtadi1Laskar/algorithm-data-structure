aaa44c975e9a03847358e123706070e28951141a
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-11-01 18:04:53 +0600

    Added Inverted Index in JavaScript

---
+++ JavaScript/Data Structure/Inverted Index/invertedIndex.js
@@ -0,0 +1,41 @@
+class InvertedIndex {
+    constructor() {
+        this.index = {};
+    }
+
+    addDocument(doc) {
+        const words = doc.Text.toLowerCase().split(' ');
+
+        for(let i = 0; i < words.length; i++) {
+            if(!this.index[words[i]]) {
+                this.index[words[i]] = [];
+            }
+            if(!words[i].includes(doc.ID)) {
+                this.index[words[i]].push(doc.ID);
+            }
+        }
+    }
+
+    contains(elem, id) {
+        for(let i = 0; i < elem.length; i++) {
+            if(elem[i] === id) {
+                return true
+            }
+        }
+        return false;
+    }
+}
+
+const invertedIndex = new InvertedIndex();
+
+const docs = [
+    {ID: 1, Text: "The quick brown fox"},
+	{ID: 2, Text: "The quick blue fox"},
+	{ID: 3, Text: "The fast red fox"}
+];
+
+for(let i = 0; i < docs.length; i++) {
+    invertedIndex.addDocument(docs[i]);
+}
+
+console.log(invertedIndex.index);

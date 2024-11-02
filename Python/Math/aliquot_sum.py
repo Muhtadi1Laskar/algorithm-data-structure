132181a3efaddc23e6d852adf67fb280607ffc84
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-11-02 19:10:00 +0600

    Added Uncheckable number function in PYthon:

--- Python/Math/aliquot_sum.py
+++ Python/Math/aliquot_sum.py
@@ -2,6 +2,13 @@ def check_aliquot(n):
     return sum([elem for elem in range(1, n) if elem % n])
 
 
+def check_untouchable(n):
+    for i in range(1, (n*n)+1):
+        if check_aliquot(i) == n:
+            return False
+    return True
+
 num = 10
 
 print(check_aliquot(num))
+print(check_untouchable(53))

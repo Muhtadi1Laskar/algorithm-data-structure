435b7435a8a7d6f121ec8c5345132da9ec58fd30
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-11-08 22:44:59 +0600

    Added the generate prime numbers function in Javascript

--- JavaScript/Math/primeNumber.js
+++ JavaScript/Math/primeNumber.js
@@ -1,12 +1,21 @@
 const isPrime = (n) => {
-    for(let i = 2; i < n; i++) {
+    for(let i = 2; i <= Math.sqrt(n); i++) {
         if(n % i === 0) {
             return false;
         }
     }
-    return true;
+    return n > 1;
 }
 
-for(let i = 1; i < 100; i++) {
-    console.log(isPrime(i));
+const generatePrime = (n) => {
+    let numbers = [];
+
+    for(let i = 2; i <= n; i++) {
+        if(isPrime(i)) {
+            numbers.push(i);
+        }
+    }
+    return numbers;
 }
+
+console.log(generatePrime(100))

62cb7aa4c5fc65c28b59879cea8ac35704cd6c4a
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-11-10 03:09:10 +0600

    Formatted the code

--- Python/Math/aliquot_sum.py
+++ Python/Math/aliquot_sum.py
@@ -1,16 +1,19 @@
 def check_aliquot(n):
     return sum([elem for elem in range(1, n) if elem % n])
 
+
 def check_untouchable(n):
-    for i in range(1, (n*n)+1):
+    for i in range(1, (n * n) + 1):
         if check_aliquot(i) == n:
             return False
     return True
+
 
 def check_abundant(n):
     if check_aliquot(n) > n:
         return True
     return False
+
 
 def check_deficient(n):
     if check_aliquot(n) < n:

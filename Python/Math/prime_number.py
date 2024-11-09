ac340c910a9bd1aecd0ac8c041ff5fc346badf97
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-11-10 03:04:56 +0600

    Formatted the code

--- Python/Math/prime_number.py
+++ Python/Math/prime_number.py
@@ -2,11 +2,12 @@ def is_prime(n):
     for i in range(2, n):
         if n % i == 0:
             return False
-    
+
     return True
 
+
 def generate_prime(n):
-    return [i for i in range(1, n+1) if is_prime(i)]
+    return [i for i in range(1, n + 1) if is_prime(i)]
 
 
 print(generate_prime(20))

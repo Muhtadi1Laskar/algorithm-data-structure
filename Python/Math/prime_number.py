b52b58be409df25893f6a78215f709e52cae3b53
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-11-12 00:11:02 +0600

    Formatted the code

--- Python/Math/prime_number.py
+++ Python/Math/prime_number.py
@@ -1,5 +1,6 @@
 import math
 
+
 def is_prime(n):
     for i in range(2, n):
         if n % i == 0:
@@ -11,6 +12,7 @@ def is_prime(n):
 def generate_prime(n):
     return [i for i in range(1, n + 1) if is_prime(i)]
 
+
 def sieve(n):
     is_prime = [True] * (n + 1)
     is_prime[0] = is_prime[1] = False
@@ -20,15 +22,16 @@ def sieve(n):
                 is_prime[j] = False
     return [i for i in range(2, n + 1) if is_prime[i]]
 
+
 def segmented_sieve(L, R):
     limit = int(math.sqrt(R)) + 1
     primes = sieve(limit)
 
     is_prime_range = [True] * (R - L + 1)
-    
+
     for prime in primes:
         start = max(prime * prime, (L + prime - 1) // prime * prime)
-        
+
         for j in range(start, R + 1, prime):
             is_prime_range[j - L] = False
 
@@ -37,7 +40,7 @@ def segmented_sieve(L, R):
 
     return [L + i for i, is_prime in enumerate(is_prime_range) if is_prime]
 
+
 L, R = 10, 50
 primes_in_range = segmented_sieve(L, R)
 print(f"Primes between {L} and {R}:", primes_in_range)
-

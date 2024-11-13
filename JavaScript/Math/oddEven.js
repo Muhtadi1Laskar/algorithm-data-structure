5f82def0ba43a22eb38a6af53393c13e0bdcb787
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-11-14 03:36:53 +0600

    Added fn2() in JavaScript

--- JavaScript/Math/oddEven.js
+++ JavaScript/Math/oddEven.js
@@ -1,6 +1,9 @@
 const fn1 = (n) => {
-    if(n % 2 == 0) {
+    if (n % 2 == 0) {
         return 'Even';
     }
     return 'Odd';
 }
+
+const fn2 = n => [...'EOVDEDN'].filter((_, i) => i % 2 === (n&1)).join('');
+

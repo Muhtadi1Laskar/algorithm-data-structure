7ee81edd97e9dc2120609c7f0e9e3d06dbb44669
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-11-17 23:12:15 +0600

    Added count vowel in Python

---
+++ Python/Recursions/count_vowel.py
@@ -0,0 +1,12 @@
+def check_vowel(char):
+    if char in "aeiou":
+        return 1
+    return 0
+
+def count_vowel(str, str_len):
+    if str_len == 1:
+        return check_vowel(str[0])
+    return count_vowel(str, str_len-1) + check_vowel(str[str_len-1]) 
+
+s = "educativeio"
+print(count_vowel(s, len(s)))

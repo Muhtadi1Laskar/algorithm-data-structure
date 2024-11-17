0f8cdff4cd2363d7259cc3dee8ea4decccc48b7b
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-11-17 23:12:32 +0600

    Formatted the code

--- Python/Recursions/count_vowel.py
+++ Python/Recursions/count_vowel.py
@@ -3,10 +3,12 @@ def check_vowel(char):
         return 1
     return 0
 
+
 def count_vowel(str, str_len):
     if str_len == 1:
         return check_vowel(str[0])
-    return count_vowel(str, str_len-1) + check_vowel(str[str_len-1]) 
+    return count_vowel(str, str_len - 1) + check_vowel(str[str_len - 1])
+
 
 s = "educativeio"
 print(count_vowel(s, len(s)))

bfcdf445e3f48cd7c37966b2de93b1cad318edf8
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-11-18 03:49:52 +0600

    Added shell sort in Haskell

--- Haskell/shellSort.hs
+++ Haskell/shellSort.hs
@@ -1,25 +1,19 @@
 module Sorts.ShellSort where
--- Shell sort uses insertion sort for sorting individual sublists
 import Sorts.InsertionSort
 
--- Produces the sequence of gaps to use for sorting a list of size n
 gaps :: Int -> [Int]
 gaps n = reverse (takeWhile (< n) tokuda)
 
--- Produces Tokuda's sequence
 tokuda :: [Int]
 tokuda = [gap n | n <- [1..]]
 
--- Computes term k in Tokuda's sequence
 gap :: (Integral a) => a -> a
 gap k = ceiling (gap' k)
 
--- Recursize helper function for gap
 gap' :: (Integral a) => a -> Double
 gap' 1 = 1
 gap' k = 2.25 * gap' (k - 1) + 1
 
--- Sort an input sequence using the shell sort algorithm
 shellSort :: (Ord a) => [a] -> [a]
 shellSort xs = shellSort' xs (gaps (length xs))
 
@@ -30,14 +24,10 @@ shellSort' [x] _ = [x]
 shellSort' l [] = l
 shellSort' l (g:gs) = shellSort' (combine [insertionSort (getSub l g i) | i <- [0..g-1]]) gs
 
--- Get the sublist of l formed by taking elements with gap g between them, starting at index i
 getSub :: (Ord a) => [a] -> Int -> Int -> [a]
 getSub [] _ _ = []
 getSub l g i = [l !! e | e <- [i, i+g..length l - 1]] 
 
-
--- Combine a list of sublists into a single list in the correct order
--- Required after individual sublists have been sorted, to rebuild the main list
 combine :: [[a]] -> [a]
 combine [] = []
 combine l@(xs:_)

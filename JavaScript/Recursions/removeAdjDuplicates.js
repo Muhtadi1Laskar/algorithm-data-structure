6605cc4bc5e440e31b63ea4599bab26d093596fb
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-11-19 18:11:37 +0600

    Added remove adjacent duplication in JavaScript

---
+++ JavaScript/Recursions/removeAdjDuplicates.js
@@ -0,0 +1,14 @@
+const removeDuplicates = (str) => {
+    if(!str) {
+        return "";
+    }
+    if(str.length === 1) {
+        return str;
+    }
+    if(str[0] === str[1]) {
+        return removeDuplicates(str.slice(1));
+    }
+    return str[0] + removeDuplicates(str.slice(1));
+}
+
+console.log(removeDuplicates("Hello Worlldd"));

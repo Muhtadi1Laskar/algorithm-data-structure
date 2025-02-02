ac45ea8efe2643560745dc3f62d826b9aa06c171
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-02-02 14:42:10 +0600

    Added the add() method to bag data structure

--- JavaScript/Data Structure/Bag/hashMapBag.js
+++ JavaScript/Data Structure/Bag/hashMapBag.js
@@ -8,7 +8,31 @@ class Bag {
         return this.count === 0;
     }
 
+    find(value) {
+        return value in this.bag;
+    }
+
     getGreatest() {
-        return this.isEmpty ? undefined : Object.keys(this.bag).sort((a, b) => a-b)[0];
+        return this.isEmpty ? undefined : Object.keys(this.bag).sort((a, b) => a - b)[0];
+    }
+
+    add(value) {
+        this.count++;
+
+        if(this.find(value)) {
+            this.bag[value]++;
+        } else {
+            this.bag[value] = 1;
+        }
     }
 }
+
+const bag = new Bag();
+
+bag.add("React");
+bag.add("React");
+bag.add("Flask");
+bag.add("Django");
+bag.add("Ruby on Rails");
+
+console.log(bag.bag);

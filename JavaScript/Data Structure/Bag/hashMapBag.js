42189326dd6e61d7cf1859af0322aff247a6cb2b
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-02-02 14:39:20 +0600

    Added getGreatest() method for bag data structure in JavaScript

--- JavaScript/Data Structure/Bag/hashMapBag.js
+++ JavaScript/Data Structure/Bag/hashMapBag.js
@@ -7,4 +7,8 @@ class Bag {
     isEmpty() {
         return this.count === 0;
     }
+
+    getGreatest() {
+        return this.isEmpty ? undefined : Object.keys(this.bag).sort((a, b) => a-b)[0];
+    }
 }

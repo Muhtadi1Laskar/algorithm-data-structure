4b82602ab58bc04746926e0d7d81ba849b2b8867
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-02-03 14:27:35 +0600

    Added add(), isEmpty() and find() methods

--- Go/Data Structure/Bag/bagHashmap.go
+++ Go/Data Structure/Bag/bagHashmap.go
@@ -4,6 +4,48 @@ import (
 	"fmt"
 )
 
+type Bags struct {
+	bag    map[string]int
+	length int
+}
+
+func newBags() *Bags {
+	return &Bags{ bag: make(map[string]int) }
+}
+
+func (b *Bags) isEmpty() bool {
+	return b.length == 0
+}
+
+func (b *Bags) find(value string) bool {
+	if _, exists := b.bag[value]; exists {
+		return true
+	}
+	return false
+}
+
+func (b *Bags) add(value string) {
+	b.length++
+	if(b.find(value)) {
+		b.bag[value]++
+	} else {
+		b.bag[value] = 1
+	}
+}
+
 func main() {
-	fmt.Println("Hello World")
+	bag := newBags()
+
+	bag.add("React")
+	bag.add("React")
+	bag.add("React")
+	bag.add("React")
+	bag.add("React")
+	bag.add("Django")
+	bag.add("Flasks")
+	bag.add("Solid.js")
+	bag.add("Ruby on Rails")
+	bag.add("Angular")
+
+	fmt.Printf("%+v\n", bag.bag)
 }

c3061ff0f9616bd7de671d889849f6311995a398
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-03-06 02:45:39 +0600

    Added the psuh() and print() method to linked list bag

--- JavaScript/Data Structure/Bag/linkedListBag.js
+++ JavaScript/Data Structure/Bag/linkedListBag.js
@@ -10,5 +10,44 @@ class LinkedList {
         this.head = null;
         this.length = 0;
     }
+
+    push(value) {
+        const newNode = new Node(value);
+
+        if(!this.head) {
+            this.head = newNode;
+            this.length += 1;
+            return;
+        }
+        let currentNode = this.head;
+
+        while(currentNode.next) {
+            currentNode = currentNode.next;
+        }
+        currentNode.next = newNode;
+        this.length++
+        return;
+    }
+
+    print() {
+        let result = [];
+        let currentNode = this.head;
+
+        for(;currentNode; currentNode = currentNode.next) {
+            result.push(currentNode.value);
+        }
+        return result.join(" ---> ")
+    }
 }
 
+const list = new LinkedList();
+
+list.push("C");
+list.push("Python");
+list.push("JavaScript");
+list.push("Ruby");
+list.push("Haskell");
+list.push("Odin");
+list.push("Assembly");
+
+console.log(list.print());

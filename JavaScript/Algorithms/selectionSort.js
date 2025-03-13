0cad3aaefa18f55810941cc51fdfa6c46c25263a
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-03-14 05:33:36 +0600

    Formatted the code

--- JavaScript/Algorithms/selectionSort.js
+++ JavaScript/Algorithms/selectionSort.js
@@ -1,12 +1,12 @@
 const selectionSort = (array) => {
-    for(let i=0;i<array.length;i++) {
+    for (let i = 0; i < array.length; i++) {
         let min = i;
-        for(let j=i+1;j<array.length;j++) {
-            if(array[min] > array[j]) {
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[min] > array[j]) {
                 min = j;
             }
         }
-        if(i !== min) {
+        if (i !== min) {
             let temp = array[i];
             array[i] = array[min];
             array[min] = temp;

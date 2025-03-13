03fa328ff3e5ffab0e219201f43f055d1d9af227
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-03-14 05:32:35 +0600

    Formatted the code

--- JavaScript/Algorithms/bubbleSort.js
+++ JavaScript/Algorithms/bubbleSort.js
@@ -1,7 +1,7 @@
 const bubbleSort = (array) => {
-    for(let i=0;i<array.length;i++) {
-        for(let j=i+1;j<array.length;j++) {
-            if(array[i] > array[j]) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[i] > array[j]) {
                 let temp = array[i];
                 array[i] = array[j];
                 array[j] = temp;

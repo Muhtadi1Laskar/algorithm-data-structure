ca1ff612f703025887fbcb522b4202960d8150d6
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-03-14 05:33:23 +0600

    Formatted the code

--- JavaScript/Algorithms/oddEvenSort.js
+++ JavaScript/Algorithms/oddEvenSort.js
@@ -1,23 +1,23 @@
 const oddEvenSort = (array) => {
     let sorted = false;
 
-    while(!sorted) {
+    while (!sorted) {
         sorted = true;
 
-        for(let i=0;i<array.length;i+=2) {
-            if(array[i] > array[i+1]) {
+        for (let i = 0; i < array.length; i += 2) {
+            if (array[i] > array[i + 1]) {
                 let temp = array[i];
-                array[i] = array[i+1];
-                array[i+1] = temp;
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
                 sorted = false;
             }
         }
 
-        for(let i=1;i<array.length;i+=2) {
-            if(array[i] > array[i+1]) {
+        for (let i = 1; i < array.length; i += 2) {
+            if (array[i] > array[i + 1]) {
                 let temp = array[i];
-                array[i] = array[i+1];
-                array[i+1] = temp;
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
                 sorted = false;
             }
         }

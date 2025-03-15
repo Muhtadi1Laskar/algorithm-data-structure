534a6a61ff4b4a37ffd5a30c371fdc6a3dfd319e
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-03-16 03:10:04 +0600

    Added comb sort

--- Python/Data Structure/Bag/array_bag.py
+++ Python/Data Structure/Bag/array_bag.py
@@ -2,15 +2,15 @@ class Bag:
     def __init__(self):
         self.bag = []
         self.len = 0
-    
+
     def push(self, value):
         self.bag.append(value)
         self.len += 1
         return
-    
+
     def get_len(self):
         return self.len
-    
+
     def log(self):
         idx = 0
 
@@ -25,16 +25,17 @@ class Bag:
             if elem == value:
                 return idx
         return None
-    
+
     def remove(self, index):
         if index < 0 or index > self.len:
             return None
-        
+
         idx = index
-        while idx < self.len-1:
-            self.bag[idx] = self.bag[idx+1] 
+        while idx < self.len - 1:
+            self.bag[idx] = self.bag[idx + 1]
             idx += 1
-        self.bag[self.len-1] = ""
+        self.bag[self.len - 1] = ""
+
 
 bag = Bag()
 

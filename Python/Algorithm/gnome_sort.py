aaed9ff91e6fe44121592d33aef840fc62317be7
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-03-16 03:09:02 +0600

    Added comb sort

--- Python/Algorithm/gnome_sort.py
+++ Python/Algorithm/gnome_sort.py
@@ -4,13 +4,14 @@ def gnome_sort(li: list[int]) -> list[int]:
     while index < len(li):
         if index == 0:
             index += 1
-        elif li[index] >= li[index-1]:
+        elif li[index] >= li[index - 1]:
             index += 1
         else:
             temp = li[index]
-            li[index] = li[index-1]
-            li[index-1] = temp
+            li[index] = li[index - 1]
+            li[index - 1] = temp
             index -= 1
     return li
+
 
 print(gnome_sort([5, 4, 3, 2, 1]))

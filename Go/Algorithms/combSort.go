a3d0de59f3aa822e026ed8582c611e996119d819
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-03-17 05:20:16 +0600

    Added comb sort in Golang

---
+++ Go/Algorithms/combSort.go
@@ -0,0 +1,24 @@
+package main
+
+func combSort(arr []int) {
+	n := len(arr)
+	gap := n
+	swapped := true
+
+	for gap > 1 || swapped {
+			gap = int(float64(gap) / 1.3)
+			if gap < 1 {
+					gap = 1
+			}
+
+			swapped = false
+			for i := 0; i < n-gap; i++ {
+					if arr[i] > arr[i+gap] {
+							temp := arr[i]
+							arr[i] = arr[i+gap]
+							arr[i+gap] = temp
+							swapped = true
+					}
+			}
+	}
+}

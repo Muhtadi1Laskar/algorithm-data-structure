624b1815c9c23272ab1dd8a224ca92874ec58612
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-03-24 05:14:30 +0600

    Formatted the code

--- JavaScript/Recursions/squareGameOnBeach.js
+++ JavaScript/Recursions/squareGameOnBeach.js
@@ -1,17 +1,17 @@
-const solve = (goal, standing, score=0, dropped=[]) => {
-  if (goal === score) {
-    return dropped;
-  } else if (score > goal || standing.length === 0) {
-    return null;
-  } else {
-    const selected = standing[0];
-    const others = standing.slice(1);
+const solve = (goal, standing, score = 0, dropped = []) => {
+    if (goal === score) {
+        return dropped;
+    } else if (score > goal || standing.length === 0) {
+        return null;
+    } else {
+        const selected = standing[0];
+        const others = standing.slice(1);
 
-    return (
-        solve(goal, others, score + selected, [...dropped, selected]) ||
-        solve(goal, others, score, dropped)
-    );
-  }
+        return (
+            solve(goal, others, score + selected, [...dropped, selected]) ||
+            solve(goal, others, score, dropped)
+        );
+    }
 };
 
 console.log(solve(50, [15, 9, 30, 21, 19, 3, 12, 6, 25, 27]));

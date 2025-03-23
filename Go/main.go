3d85348291feb3e203b57ca41b57957576e397e3
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-03-23 06:19:47 +0600

    Added a stack data structure

--- Go/main.go
+++ Go/main.go
@@ -4,6 +4,85 @@ import (
 	"fmt"
 )
 
+type Node struct {
+	value string
+	next *Node
+}
+
+type Stack struct {
+	head *Node
+	length int
+}
+
+func NewStack() *Stack {
+	return &Stack{
+		head: nil,
+		length: 0,
+	}
+}
+
+func (s *Stack) Push(value string) {
+	newNode := &Node{
+		value: value,
+		next: s.head,
+	}
+
+	s.head = newNode
+	s.length++
+	return
+}
+
+func (s *Stack) Pop() {
+	if s.head == nil {
+		fmt.Println("The stack is empty")
+		return
+	}
+
+	s.head = s.head.next
+	s.length--
+	return
+}
+
+func (s *Stack) Peak() string {
+	if s.head == nil {
+		return "The stack is empty"
+	}
+	return s.head.value
+}
+
+func (s *Stack) Print() []string {
+	if s.head == nil {
+		fmt.Println("The stack is empty")
+		return nil
+	}
+	result := make([]string, s.length)
+	currentNode := s.head
+
+	for i := 0; currentNode != nil; i++ {
+		result[i] = currentNode.value
+		currentNode = currentNode.next
+	}
+	return result
+}
+
+
 func main() {
-	fmt.Println("Hello World")
+	stack := NewStack()
+
+	stack.Push("x = 10")
+	stack.Push("y = 20")
+	stack.Push("z = 40")
+	stack.Push("m = 50")
+	stack.Push("n = 60")
+	stack.Push("k = 70")
+	stack.Push("l = 80")
+
+	fmt.Println(stack.Print())
+	fmt.Println(stack.head.value)
+
+	stack.Pop()
+
+	fmt.Println(stack.Print())
+
+	fmt.Println(stack.Peak())
 }

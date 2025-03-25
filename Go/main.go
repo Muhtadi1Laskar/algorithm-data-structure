510334aeec514b6f424bd715f4a15310e333d3a0
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-03-25 21:44:35 +0600

    Test

--- Go/main.go
+++ Go/main.go
@@ -4,6 +4,23 @@ import (
 	"fmt"
 )
 
+func solve(goal int, standing []int, score int, dropped []int) []int {
+	if goal == score {
+		return dropped
+	} else if score > goal || len(standing) == 0 {
+		return nil
+	} else {
+		selected := standing[0]
+		others := standing[1:]
+
+		if result := solve(goal, others, selected + score, append(append([]int{}, dropped...), selected)); result != nil {
+			return result
+		}
+		return solve(goal, others, score, dropped)
+	}
+}
+
 func main() {
-	fmt.Println("Hello World")
+	result := solve(50, []int{15, 9, 30, 21, 19, 3, 12, 3, 2, 25, 27}, 0, []int{})
+	fmt.Println(result)
 }

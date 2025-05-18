8220e7e65d2928641efcadbec708a8f6ab84e701
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-05-18 13:23:34 +0600

    Selection Sort

--- Go/main.go
+++ Go/main.go
@@ -4,6 +4,26 @@ import (
 	"fmt"
 )
 
+func selectionSort(array []int) []int {
+	for i := 0; i < len(array); i++ {
+		var min int = i
+
+		for j := i+1; j < len(array); j++ {
+			if array[min] > array[j] {
+				min = j
+			}
+		}
+		if min != i {
+			temp := array[i]
+			array[i] = array[min]
+			array[min] = temp
+		}
+	}
+	return array
+}
+
 func main() {
-	fmt.Println("Hello World")
+	array := []int{5, -4, 3, 2, 1}
+
+	fmt.Println(selectionSort(array))
 }

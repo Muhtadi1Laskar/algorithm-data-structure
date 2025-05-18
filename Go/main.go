35727f4a009a39d5c2e3f9509d629aebb2e3851a
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-05-18 13:16:16 +0600

    Insertion Sort

--- Go/main.go
+++ Go/main.go
@@ -4,6 +4,23 @@ import (
 	"fmt"
 )
 
+func insertionSort(array []int) []int {
+	for i := 0; i < len(array); i++ {
+		key := array[i]
+		j := i - 1
+
+		for j >= 0 && array[j] > key {
+			array[j+1] = array[j]
+			j--
+		}
+		array[j+1] = key
+	}
+	return array
+}
+
 func main() {
-	fmt.Printf("Hello World\n")
+	array := []int{5, -
+		4, -3, 2, 1}
+
+	fmt.Println(insertionSort(array))
 }

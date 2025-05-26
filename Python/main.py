8cf67621bdbaef7a4adc4bd230e9d4493e21801e
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-05-27 00:00:07 +0600

    Test

--- Python/main.py
+++ Python/main.py
@@ -1,1 +1,14 @@
-print("Hello World")
+def insertion_sort(li):
+    for i in range(1, len(li)):
+        key = li[i]
+        j = i - 1
+
+        while j >= 0 and li[j] > key:
+            li[j+1] = li[j]
+            j -= 1
+        
+        li[j+1] = key
+    
+    return li
+
+print(insertion_sort([5, 4, 3, 2, 1]))

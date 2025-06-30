01af8c85b50bd991af8d6363465256cd7a1a9326
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-07-01 03:34:29 +0600

    Added the hash function

--- Nim/main.nim
+++ Nim/main.nim
@@ -1,11 +1,28 @@
+import strutils
+import math
+
 type
     HashMap = ref object
         arr: seq[seq[string]]
         size: int
+
+proc hash(key: string): int = 
+    var hashs: int = 0
+
+    for i in key:
+        hashs = ((hashs shl  5) - hashs) + ord(i)
+        hashs = hashs and 0xffffffff
+    
+    result = abs(hashs)
+    
 
 proc new_hash_map(): HashMap = 
     return HashMap(arr: @[@[]], size: 0)
 
 when isMainModule:
     var hash_map = new_hash_map()
+
+    var h = hash("This is a messages")
+
+    echo h
     

d52e23518c4c1903450bc623eccd50a0d1590743
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-07-01 03:57:24 +0600

    Added the set() method for hash map implementation in nim

--- Nim/main.nim
+++ Nim/main.nim
@@ -1,28 +1,47 @@
-import strutils
-import math
+import strutils, math, sequtils
 
 type
-    HashMap = ref object
-        arr: seq[seq[string]]
-        size: int
+  HashMap = ref object
+    arr: seq[seq[string]]
+    size: int
 
-proc hash(key: string): int = 
-    var hashs: int = 0
+proc hash(self: HashMap, key: string): int =
+  var hashs = 0
+  for ch in key:
+    hashs = ((hashs shl 5) - hashs) + ord(ch)
+    hashs = hashs and 0xffffffff
+  result = abs(hashs)
 
-    for i in key:
-        hashs = ((hashs shl  5) - hashs) + ord(i)
-        hashs = hashs and 0xffffffff
-    
-    result = abs(hashs)
-    
+proc new_hash_map(): HashMap =
+  result = HashMap(arr: newSeqWith(16, newSeq[string]()), size: 16)
 
-proc new_hash_map(): HashMap = 
-    return HashMap(arr: @[@[]], size: 0)
+proc set(self: HashMap, key: string, value: string) =
+  let index = self.hash(key) mod self.size
+  var bin = self.arr[index]
+
+  for i in 0 ..< bin.len:
+    if bin[i] == key:
+      bin[i] = value
+      self.arr[index] = bin
+      return
+
+  bin.add(key)
+  bin.add(value)
+  self.arr[index] = bin
 
 when isMainModule:
-    var hash_map = new_hash_map()
+  let hash_map = new_hash_map()
 
-    var h = hash("This is a messages")
+  let data = [
+    ("Name", "Luffy"),
+    ("Occupation", "Pirate"),
+    ("Age", "20"),
+    ("Favourite Food", "Meat"),
+    ("Devil Fruit User", "true"),
+    ("Devil Fruit Name", "Gomu Gomu No Mi")
+  ]
 
-    echo h
-    
+  for (k, v) in data:
+    hash_map.set(k, v)
+
+  echo hash_map.arr

213bca09c283a2229b0399a86ccde9ae6a126e94
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-07-01 03:21:39 +0600

    Added the template for array hash map implementation

---
+++ Nim/main.nim
@@ -0,0 +1,11 @@
+type
+    HashMap = ref object
+        arr: seq[seq[string]]
+        size: int
+
+proc new_hash_map(): HashMap = 
+    return HashMap(arr: @[@{], size: 0)
+
+when isMainModule:
+    var hash_map = new_hash_map()
+    

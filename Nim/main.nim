8264d0863c2537f84a4777cc34cfbbb8be3e490f
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-07-01 03:22:12 +0600

    Fixed bug

--- Nim/main.nim
+++ Nim/main.nim
@@ -4,7 +4,7 @@ type
         size: int
 
 proc new_hash_map(): HashMap = 
-    return HashMap(arr: @[@{], size: 0)
+    return HashMap(arr: @[@[]], size: 0)
 
 when isMainModule:
     var hash_map = new_hash_map()

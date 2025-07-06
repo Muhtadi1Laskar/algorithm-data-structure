552d30fca7927d278b0ec7aa00f28e6764ba9bd5
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-07-07 00:49:30 +0600

    Added insert() method for Binary Search Tree in Nim

--- Nim/Data Structure/Tree/BinarySearchTree.nim
+++ Nim/Data Structure/Tree/BinarySearchTree.nim
@@ -1,3 +1,5 @@
+import strutils
+
 type
     Node = ref object
         left: Node
@@ -6,8 +8,37 @@ type
 
 type
     BinarySearchTree = ref object
-        root Node
+        root: Node
 
+proc new_bst(): BinarySearchTree =
+    return BinarySearchTree(root: Node(left: nil, right: nil))
 
-proc new_bst() BinarySearchTree =
-    return BinarySearchTree()
+proc insert(bst: BinarySearchTree, node: Node, value: string) = 
+    if value.len < node.value.len:
+        if node.left.isNil:
+            node.left = Node(value: value)
+        else:
+            bst.insert(node.left, value)
+    else:
+        if node.right.isNil:
+            node.right = Node(value: value)
+        else:
+            bst.insert(node.right, value)
+
+proc Insert(bst: BinarySearchTree, value: string) = 
+    if bst.root.isNil:
+        bst.root = Node(value: value)
+    else:
+        bst.insert(bst.root, value)
+
+when isMainModule:
+    let bst = BinarySearchTree()
+
+    bst.Insert("C")
+    bst.Insert("JavaScript")
+    bst.Insert("Python")
+    bst.Insert("Odin")
+    bst.Insert("Haskell")
+    bst.Insert("Machine Code")
+
+    echo bst.root.right.value

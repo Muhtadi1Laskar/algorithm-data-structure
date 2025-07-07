2d4860a904d78995ffa54b372734bbe7c0d580fe
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-07-08 01:13:11 +0600

    Added recursive solution for Fibonacci 3 problem

--- Nim/main.nim
+++ Nim/main.nim
@@ -1,7 +1,20 @@
+import tables
+
 proc fib(n: int): int = 
     if n <= 2:
         return 1
     return fib(n-1) + fib(n-2)
+
+func fibMemo(n: int, memo: var Table[int, int]): int =
+    if n in memo:
+        return memo[n]
+
+    if n <= 2:
+        return 1
+
+    memo[n] = fibMemo(n - 1, memo) + fibMemo(n - 2, memo)
+
+    return memo[n]
 
 proc fib_iter(n: int): int = 
     if n <= 0:
@@ -24,6 +37,8 @@ proc fib_iter(n: int): int =
 
 
 when isMainModule:
-    let n: int = 10
+    var memoTable = initTable[int, int]() # Initialize an empty table for memoization
 
-    echo fib_iter(n)
+    echo "Fibonacci(10): ", fibMemo(10, memoTable)
+    echo "Fibonacci(20): ", fibMemo(20, memoTable)
+    echo "Fibonacci(50): ", fibMemo(50, memoTable)

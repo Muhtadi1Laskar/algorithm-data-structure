40d74a3130d8338fdfa198975cc3469d0f241e7a
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-07-08 01:10:16 +0600

    Added recursive solution for Fibonacci 2 problem

--- Nim/main.nim
+++ Nim/main.nim
@@ -3,8 +3,27 @@ proc fib(n: int): int =
         return 1
     return fib(n-1) + fib(n-2)
 
+proc fib_iter(n: int): int = 
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+
+    var
+        a = 0
+        b = 1
+        sum = 1 
+
+    for i in 2..n:
+        let next = a + b
+        sum += next
+        a = b
+        b = next
+
+    return sum
+
 
 when isMainModule:
     let n: int = 10
 
-    echo fib(n)
+    echo fib_iter(n)

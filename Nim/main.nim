7df0e4aa136e944af4b15b8c0562cc20422194c1
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-07-13 20:05:11 +0600

    Added the push method for static stack implementation

---
+++ Nim/main.nim
@@ -0,0 +1,27 @@
+const SIZE: int = 5
+
+type 
+    Stack[T] = ref object
+        stack: array[SIZE, T]
+        top: int
+
+proc new_stack[T](): Stack[T] = 
+    return Stack[T](stack: default(array[SIZE, T]), top: -1)
+
+proc push[T](stack: Stack[T], item: T) = 
+    if stack.top == SIZE-1:
+        echo "Index out of range"
+        return
+    stack.top += 1
+    stack.stack[stack.top] = item
+
+when isMainModule:
+    let stack: Stack[string] = new_stack[string]()
+
+    stack.push("React.js")
+    stack.push("Angular")
+    stack.push("Solid.js")
+    stack.push("Apline.js")
+    stack.push("Django")
+
+    echo stack.stack

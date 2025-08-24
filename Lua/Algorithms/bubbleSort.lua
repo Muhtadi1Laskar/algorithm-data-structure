2689cfb83465116b9ed713f3ca4aed8c8e4f4d15
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-08-24 22:34:20 +0600

    Added bubble sort in Lua

---
+++ Lua/Algorithms/bubbleSort.lua
@@ -0,0 +1,15 @@
+function bubbleSort(array)
+    for i = 1, #array, 1 do
+        for j = i + 1, #array, 1 do
+            if array[i] > array[j] then 
+                temp = array[i]
+                array[i] = array[j]
+                array[j] = temp
+            end
+        end
+    end
+    return array
+end
+
+
+print(table.concat( bubbleSort({5, 4, 3, 2, 1}), ", "))

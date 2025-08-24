dcbe499d9c421bb762dc8477bfff4192db2fab1e
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-08-25 01:41:42 +0600

    Formatted the code

--- Lua/Algorithms/combSort.lua
+++ Lua/Algorithms/combSort.lua
@@ -2,26 +2,26 @@ function combSort(array)
     local n = #array
     local gap = n
     local swapped = true
-
+    
     while gap > 1 or swapped do
         gap = math.floor(gap / 1.3)
-
+        
         if gap < 1 then
             gap = 1
         end
-
+        
         swapped = false
-
-        for i = 1, n-gap, 1 do
-            if array[i] > array[i+gap] then
+        
+        for i = 1, n - gap, 1 do
+            if array[i] > array[i + gap] then
                 temp = array[i]
-                array[i] = array[i+gap]
-                array[i+gap] = temp
+                array[i] = array[i + gap]
+                array[i + gap] = temp
                 swapped = true
             end
         end
     end
-
+    
     return array
 end
 

a22c7bc98c1362531061ee84e8bc09ccf8253b1d
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-08-26 00:28:47 +0600

    Formatted the code

--- Lua/Algorithms/shuttleSort.lua
+++ Lua/Algorithms/shuttleSort.lua
@@ -1,12 +1,12 @@
 function shuttleSort(array)
     for i = 2, #array, 1 do
         local j = i
-
-        while j > 1 and array[j-1] > array[j] do
+        
+        while j > 1 and array[j - 1] > array[j] do
             local temp = array[j]
-            array[j] = array[j-1]
-            array[j-1] = temp
-             j = j - 1
+            array[j] = array[j - 1]
+            array[j - 1] = temp
+            j = j - 1
         end
     end
     return array

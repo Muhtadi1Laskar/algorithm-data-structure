916b6fbc9e02eb8ad22a4f9a1fb0acc6cd50c43e
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-09-19 04:45:02 +0600

    Implemented bogo sort in Julia

---
+++ Julia/Algorithms/bogo_sort.jl
@@ -0,0 +1,21 @@
+function is_sorted(arr)
+    for i in 1:(length(arr) - 1)
+        if arr[i] > arr[i+1]
+            return false
+        end
+    end
+    return true
+end
+
+function bogosort!(arr)
+    while !is_sorted(arr)
+        shuffle!(arr)
+    end
+    return arr
+end
+
+# Example usage:
+my_array = [3, 1, 4, 1, 5, 9, 2, 6]
+println("Original array: ", my_array)
+bogosort!(my_array)
+println("Sorted array: ", my_array)

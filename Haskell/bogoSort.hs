32ea13828cf50275c6330467d907216c2a90d2b8
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-09-19 04:46:09 +0600

    Implemented bogo sort in Julia

---
+++ Haskell/bogoSort.hs
@@ -0,0 +1,22 @@
+import System.Random (randomRIO, newStdGen)
+import Data.List (permutations, sort)
+import Control.Monad (replicateM)
+
+isSorted :: Ord a => [a] -> Bool
+isSorted [] = True
+isSorted [_] = True
+isSorted (x:y:xs) = x <= y && isSorted (y:xs)
+
+shuffle :: [a] -> IO [a]
+shuffle xs = do
+    gen <- newStdGen
+    let n = length xs
+    indices <- replicateM n (randomRIO (0, n - 1))
+    return $ map (\i -> xs !! i) (take n (permutations indices !! 0)) -- Simplified for demonstration
+
+bogosort :: Ord a => [a] -> IO [a]
+bogosort xs = do
+    shuffledList <- shuffle xs
+    if isSorted shuffledList
+        then return shuffledList
+        else bogosort xs

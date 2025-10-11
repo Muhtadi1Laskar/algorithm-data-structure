056a4b2b2bb51ef42e2df26cf2af2db05f5d7416
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-10-12 00:47:53 +0600

    Added the hash function to hash data on Merkle Trees

--- Nim/main.nim
+++ Nim/main.nim
@@ -1,1 +1,12 @@
-echo "Hello World"
+import nimcrypto
+
+proc hashData(data: string): string = 
+    return toHex(sha256.digest(data).data, lowercase=true)
+
+
+
+
+when isMainModule:
+    let data: string = "This is a string"
+
+    echo hashData(data)

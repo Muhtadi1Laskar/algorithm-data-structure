153f5410fdeee0c2ba64f61e583366b294f79b50
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-10-12 15:41:37 +0600

    Added build function in Merkle Tree

--- Go/main.go
+++ Go/main.go
@@ -16,6 +16,45 @@ func hashFunction(text string) string {
 	return hex.EncodeToString(hashedBytes)
 }
 
+type MerkleTree struct {
+	leaves []string
+	levels [][]string
+	root string
+}
+
+func (tree *MerkleTree) build(leaves []string) [][]string {
+	if len(leaves) == 0 {
+		return [][]string{}
+	}
+	var levels [][]string = [][]string{leaves}
+	var currentLevel []string = leaves
+
+	for len(currentLevel) > 0 {
+		var nextLevel []string = []string{}
+		for i := 0; i < len(currentLevel) - 1; i += 2 {
+			if i + 1 < len(currentLevel) {
+				hash := hashFunction(currentLevel[i] + currentLevel[i+1])
+				nextLevel = append(nextLevel, hash)
+			} else {
+				nextLevel = append(nextLevel, hashFunction(currentLevel[i] + currentLevel[i]))
+			}
+		}
+		levels = append(levels, nextLevel)
+		currentLevel = nextLevel
+	}
+	return levels
+}
+
+func newMerkleTree(leaves []string) *MerkleTree {
+	tree := &MerkleTree{leaves: leaves} 
+	tree.levels = tree.build(leaves)
+	
+	if len(leaves) > 0 {
+		tree.root = tree.levels[len(tree.levels)][0]
+	}
+	return tree
+}
+
 func main() {
 	var data string = "This is a text"
 

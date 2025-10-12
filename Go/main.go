4f58200726bf3e45cb5a789336ae5230413604fe
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-10-12 15:05:23 +0600

    Added the hash function to hash data on Merkle Trees in Golang

--- Go/main.go
+++ Go/main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 )
 
+func hashFunction(text string) string {
+	byteMessage := []byte(text)
+	hash := sha256.New()
+	hash.Write(byteMessage)
+
+	hashedBytes := hash.Sum(nil)
+
+	return hex.EncodeToString(hashedBytes)
+}
+
 func main() {
-	fmt.Println("Hello World")
+	var data string = "This is a text"
+
+	fmt.Println(hashFunction(data))
 }

9e35e0af6ad682cbf62e863cd224362726862952
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2025-12-11 00:35:44 +0600

    Formatted the code

--- Go/main.go
+++ Go/main.go
@@ -4,26 +4,6 @@ import (
 	"fmt"
 )
 
-func binarySearch(array []string, item string) int {
-	var l int = 0
-	var r int = len(array) - 1
-
-	for l <= r {
-		mid := l + (r-l)/2
-
-		if array[mid] == item {
-			return mid
-		} else if array[mid] > item {
-			r = mid - 1
-		} else {
-			l = mid + 1
-		}
-	}
-	return -1
-}
-
 func main() {
-	var array []string = []string{"Assembly", "C", "JavaScript", "Machine Code", "Python"}
-
-	fmt.Println(binarySearch(array, "C"))
+	fmt.Println("Hello World")
 }

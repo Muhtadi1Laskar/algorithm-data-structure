7393196442bfe16577372a22c3543369b1c08202
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2026-06-23 00:10:35 +0600

    Formatted the code

--- Go/main.go
+++ Go/main.go
@@ -4,6 +4,18 @@ import (
 	"fmt"
 )
 
+func factorial(x int) int {
+	var result int = 1
+
+	for i := x; i >= 2; i-- {
+		result *= i
+	}
+	return result
+}
+
 func main() {
-	fmt.Println("Hello World")
+	// var array [6]int = [6]int{5, 4, 3, 55, 2, 1}
+
+	// fmt.Println(insertionSort(array[:]))
+	fmt.Printf("%d", factorial(7))
 }

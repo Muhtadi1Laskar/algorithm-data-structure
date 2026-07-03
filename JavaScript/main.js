66064ea04c6c8d8f1e18e26d52b954b85dd7a417
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2026-07-03 12:41:02 +0600

    Re-structured the C files

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,18 +1,1 @@
-const DOLLAR_TO_BDT_RATE = 123.28;
-
-const calculateProfit = (totalCost, sellingPrice, soldAmount) => {
-    const perPersonCost = Math.floor(totalCost / 3);
-    const sellingPriceInBDT = (sellingPrice * soldAmount) * DOLLAR_TO_BDT_RATE;
-    console.log(sellingPriceInBDT / 3);
-    const perPersonProfit = Math.floor(sellingPriceInBDT / 3) - perPersonCost;
-    const profitPercentage = (perPersonProfit / perPersonCost) * 100
-    return [perPersonProfit, profitPercentage];
-}
-
-const totalOperaionCost = 11350;
-const amountOfProductSold = 50;
-const sellingPrice = 20;
-
-console.log(calculateProfit(totalOperaionCost, sellingPrice, amountOfProductSold));
-
-
+console.log("Hello World");

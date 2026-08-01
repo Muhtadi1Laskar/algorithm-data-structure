38c55b156200817ccaa4489266541bcddbb61ad9
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2026-08-02 05:00:48 +0600

    Formatted the code

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+const insertionSort = (array) => {
+    for(let i = 1; i < array.length; i++) {
+        let key = array[i];
+        let  j = i - 1;
+
+        while(j >= 0 && array[j] > key) {
+            array[j+1] = array[j];
+            j--;
+        }
+        array[j+1] = key;
+    }
+    return array;
+}
+
+console.log(insertionSort([5, -4, 3, 2, 1]));
